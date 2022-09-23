7d9e5fcab4581276b06849af7ab295a8e3540bd2
Jennifer Man <jenniferman702@gmail.com>
2022-09-23 12:34:43 -0400

    commit tree 96e1513fb0ab3af95290b052fff8acffaadfd530

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
+// function addEventListener(){
+// const input = document.getElementById('button');
+
+// function clickAlert() {
+//   alert('I was clicked!');
+// }
+
+// input.addEventListener('click', clickAlert);
+// }
+// console.log(document.querySelector('form'));
+// function addingEventListener (clickAlert){
+//     return('I was clicked!');
+// 
+// input.addEventListener('click',clickAlert);
+
+
 function addingEventListener() {
-}
+  
+    const input = document.getElementById('button');
+    
+    function clickAlert() {
+      alert('I was clicked!');
+    }
+    
+    input.addEventListener('click', clickAlert);
+    
+    }
+    console.log(document.querySelector('form'));
